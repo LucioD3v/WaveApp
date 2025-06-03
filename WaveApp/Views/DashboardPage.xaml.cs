d5795d69d1d2ba43b50f0039a78d7f294c14b551
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-06-03 16:40:10 -0600

    Se agrega Popup para mostrar el Calculo Matematico de las Olas

--- WaveApp/Views/DashboardPage.xaml.cs
+++ WaveApp/Views/DashboardPage.xaml.cs
@@ -1,9 +1,23 @@
+﻿
 namespace WaveApp.Views;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage()
-	{
-		InitializeComponent();
-	}
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnInfoIconClicked(object sender, EventArgs e)
+    {
+        // Show the popup
+        await DisplayAlert("Mathematical calculation of wave energy",
+            "P = (ρ · g² · H² · T) / (64π)\n\n" +
+            "Where:\n" +
+            "ρ: Density of water (approximately 1025kg/m³)\n" +
+            "g: Acceleration of gravity (9.81 m/s²)\n" +
+            "H: Significant wave height\n" +
+            "T: Period of the waves",
+            "Close");
+    }
 }
